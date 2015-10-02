1ef35615f5ee01bc7d7403aeb574ae506296c70b
laazer <brandt.j@husky.neu.edu>
2015-10-02 12:10:12 -0400

    everthing is awsome, aka split string works

--- scrap.cpp
+++ scrap.cpp
@@ -24,12 +24,11 @@ string repeat()
 
 char ** splitString(const char words[], char ** buff) {
     string s = words;
-    //char * result[w_cnt];
     char ** pt_result = buff;
     int index, j = 0;
     bool addWord = false;
-    for(int i = 0; i <= s.length(); i++) {
-        if(s[i] == ' ') {
+    for(int i = 0; i < s.length(); i++) {
+        if(s[i] == ' ' || (i + 1) == s.length()) {
             if(addWord) {
                 string tmp = s.substr(j, i - j);
                 //+ 1 for null char
@@ -60,43 +59,6 @@ int cntSpace(string s) {
     return x;
 }
 
-//char **strsplit(const char* str, const char* delim, size_t* numtokens) {
-//    // copy the original string so that we don't overwrite parts of it
-//    // (don't do this if you don't need to keep the old line,
-//    // as this is less efficient)
-//    char *s = strdup(str);
-//
-//    // these three variables are part of a very common idiom to
-//    // implement a dynamically-growing array
-//    size_t tokens_alloc = 1;
-//    size_t tokens_used = 0;
-//    char **tokens = calloc(tokens_alloc, sizeof(char*));
-//
-//    char *token, *strtok_ctx;
-//    for (token = strtok_r(s, delim, &strtok_ctx);
-//            token != NULL;
-//            token = strtok_r(NULL, delim, &strtok_ctx)) {
-//        // check if we need to allocate more space for tokens
-//        if (tokens_used == tokens_alloc) {
-//            tokens_alloc *= 2;
-//            tokens = realloc(tokens, tokens_alloc * sizeof(char*));
-//        }
-//        tokens[tokens_used++] = strdup(token);
-//    }
-//
-//    // cleanup
-//    if (tokens_used == 0) {
-//        free(tokens);
-//        tokens = NULL;
-//    } else {
-//        tokens = realloc(tokens, tokens_used * sizeof(char*));
-//    }
-//    *numtokens = tokens_used;
-//    free(s);
-//
-//    return tokens;
-//}
-
 int print_array(char ** words, int len) {
     int i = 0;
     while(i < len) {
@@ -112,7 +74,7 @@ int main()
     char * list[] = {"abc", "i", "djaksdj", "sdsakdjasadasd", "hhjh"};
     const char test[] = "abc def jkjk lmnop";
     string stest = test;
-    char * tbuff[100];
+    char * tbuff[4];
     print_array(splitString(test, tbuff), 4);
     //cout << cntSpace(stest) << endl;
     cout << "True " << true << " False " << false << endl;
