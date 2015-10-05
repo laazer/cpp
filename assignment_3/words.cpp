c2f88ce16c52355f5c00434a756c31ce756a881d
laazer <brandt.j@husky.neu.edu>
2015-10-04 23:37:44 -0400

    added main for debugging

--- assignment_3/words.cpp
+++ assignment_3/words.cpp
@@ -202,3 +202,29 @@ removeWord(Words *p_w, const char word[])
     p_w->num_words = p_w->num_words - cnt;
     return cnt;
 }
+
+//TODO remember to remove
+int
+main()
+{
+  Words * w1 = newList(10);
+  appendList(w1, "caterpie raichu butterfree");
+  printList(w1); // caterpie raichu butterfree
+
+  Words * w2 = newList("charmeleon mewtwo pikachu charmander");
+  printList(w2); // charmeleon mewtwo pikachu charmander
+
+  appendList(w2, w1);
+  printList(w2); // charmeleon mewtwo pikachu charmander caterpie raichu butterfree
+
+  appendList(w2, "abra");
+  printList(w2); // charmeleon mewtwo pikachu charmander caterpie raichu butterfree abra
+
+  removeWord(w2, "charmeleon");
+  printList(w2); // mewtwo pikachu charmander caterpie raichu butterfree abra
+
+  deleteList(w1);
+  deleteList(w2);
+
+  return 0;
+}
