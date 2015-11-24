a8b28bbafa0af5fcc1bd6e08c496b2e7faaae3ff
laazer <brandt.j@husky.neu.edu>
2015-11-24 11:29:51 -0500

    ex1 with boost

--- assignment_6/example1.cpp
+++ assignment_6/example1.cpp
@@ -1,19 +1,34 @@
 #include <cstdlib>
 #include <string>
 #include <vector>
+//#include <boost/numeric/ublas/vector.hpp>
+//#include <boost/fusion/algorithm/transformation/push_back.hpp>
+//#include <boost/fusion/include/push_back.hpp>
+#include <boost/interprocess/containers/vector.hpp>
+
 using namespace std;
+//namespace vboost = boost::numeric::ublas;
+//namespace aboost = boost::fusion;
+namespace vboost = boost::interprocess;
+
 
 int main() {
   string * s1 = new string("mario");
   string * s2 = new string("luigi");
   string * s3 = new string("toad");
 
-  vector<string *> v1;
+  vboost::vector<string *> v1;
+//  aboost::push_back(v1, s1);
+//  aboost::push_back(v1, s2);
+//  aboost::push_back(v1, s3);
+
   v1.push_back(s1);
   v1.push_back(s2);
   v1.push_back(s3);
 
-  vector<string *> v2;
+  vboost::vector<string *> v2;
+
+//  aboost::push_back(v2, v1[rand() % v1.size()]);
   v2.push_back(v1[rand() % v1.size()]);
 
   return 0;
